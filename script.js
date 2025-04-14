70e8a66b17403a22d4cde2c63e5574da80c47c9b
Mohamed Talhaoui <144726758+mohamedtalhaouii@users.noreply.github.com>
2025-04-14 18:52:38 +0100

    add rotation effect and fuction for reset btn

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-// Noms des Binômes : Mohamed Talhaoui & Fatima Boumsahi
-// Filière : Informatique et IA (S4) | 2024/2025 
-
 let moves = document.querySelector("#moves");
 let totalPairs = document.querySelector("#total-pairs");
 let cards = document.querySelectorAll(".card");
+let cardFront = document.querySelector(".card-front");
+let cardBack = document.querySelector(".card-back");
 let difficulty = document.querySelector("#difficulty");
 let gameBoard = document.querySelector(".game-board");
+let resetBtn = document.querySelector("#reset-btn");
 
-
-// Difficulte
+// Niveau de difficulte
 difficulty.onchange = function(){
     for (let i = 0; i < difficulty.options.length; i++) {
         if (difficulty.options[i].selected) {
@@ -23,9 +22,28 @@ difficulty.onchange = function(){
     }   
 };
 
+// Rotation cardes effect
+for (let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener("click", function(){
+        cards[i].style.cssText = `
+            transform: rotateY(180deg);
+        `;
+    })
+}
+
 // Mouvements 
 cards.forEach(card => {
     card.onclick = function(){
         moves.textContent++;
     }
 });
+
+// Reset button
+resetBtn.onclick = function() {
+    moves.textContent = 0;
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].style.cssText = `
+            transform: rotateY(0);
+        `;
+    }
+}
