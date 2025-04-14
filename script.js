69e8ec751e7a710e2403ef121c0ad69ce98ce19c
Mohamed Talhaoui <144726758+mohamedtalhaouii@users.noreply.github.com>
2025-04-14 15:38:05 +0100

    add some functions in javascript

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 // Noms des Binômes : Mohamed Talhaoui & Fatima Boumsahi
 // Filière : Informatique et IA (S4) | 2024/2025 
 
-// Pas Encore...
+let moves = document.querySelector("#moves");
+let totalPairs = document.querySelector("#total-pairs");
+let cards = document.querySelectorAll(".card");
+let difficulty = document.querySelector("#difficulty");
+let gameBoard = document.querySelector(".game-board");
+
+
+// Difficulte
+difficulty.onchange = function(){
+    for (let i = 0; i < difficulty.options.length; i++) {
+        if (difficulty.options[i].selected) {
+            totalPairs.textContent = difficulty.options[i].value;
+            // change template of grid
+            gameBoard.style.cssText = `
+                grid-template-columns: repeat(${(difficulty.options[i].value)/2}, 1fr);
+            `;
+        }
+    }   
+};
+
+// Mouvements 
+cards.forEach(card => {
+    card.onclick = function(){
+        moves.textContent++;
+    }
+});
