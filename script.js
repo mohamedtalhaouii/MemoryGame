b9f9b35a0dbe44bfc597b08f85a2e5ff906ee84b
Mohamed Talhaoui <144726758+mohamedtalhaouii@users.noreply.github.com>
2025-04-15 17:57:31 +0100

    add sessionStorage to website to save infos while refreching, and same essentiel functionality to make game better

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ difficulty.onchange = function(){
                 grid-template-columns: repeat(${(difficulty.options[i].value)/2}, 1fr);
             `;
         }
-    }   
+    }
 };
 
 // Rotation cardes effect
@@ -27,15 +27,36 @@ for (let i = 0; i < cards.length; i++) {
         cards[i].style.cssText = `
             transform: rotateY(180deg);
         `;
+        cards[i].children[0].style.cssText = `
+            visibility: hidden;
+            opacity: 0;
+            transition: visibility 0s, opacity 0.5s linear;
+        `;
+        cards[i].children[1].style.cssText = `
+            visibility: visible;
+            opacity: 1;
+        `;
+        sessionStorage.setItem(`Card ${i}`, cards[i].style.cssText);
+        sessionStorage.setItem(`Card Front ${i}`, cards[i].children[0].style.cssText);
+        sessionStorage.setItem(`Card Back ${i}`, cards[i].children[1].style.cssText);
     })
+    cards[i].style.cssText = sessionStorage.getItem(`Card ${i}`);
+    cards[i].children[0].style.cssText = sessionStorage.getItem(`Card Front ${i}`);
+    cards[i].children[1].style.cssText = sessionStorage.getItem(`Card Back ${i}`);
 }
+
 
 // Mouvements 
 cards.forEach(card => {
     card.onclick = function(){
         moves.textContent++;
+        sessionStorage.setItem("Mouvements", moves.textContent);
     }
+    // Bach fach ndir refrech may3tilich lkhawi f blasa dyal Mouvements
+    sessionStorage.getItem("Mouvements") !== null ? moves.textContent = sessionStorage.getItem("Mouvements") : null;
 });
+
+
 
 // Reset button
 resetBtn.onclick = function() {
@@ -43,10 +64,20 @@ resetBtn.onclick = function() {
 }
 
 function resetAll(){
-    moves.textContent = 0;
+    sessionStorage.clear();
+    moves.textContent = "0";
     for (let i = 0; i < cards.length; i++) {
         cards[i].style.cssText = `
             transform: rotateY(0);
+        `;
+        cards[i].children[0].style.cssText = `
+            visibility: visible;
+            opacity: 1;
+        `;
+        cards[i].children[1].style.cssText = `
+            visibility: hidden;
+            opacity: 0;
+            transition: visibility 0s, opacity 1s linear;
         `;
     }
 }
