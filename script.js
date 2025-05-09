2680941ef79722703dfdf8e243e814f8bce3b97b
Mohamed Talhaoui <144726758+mohamedtalhaouii@users.noreply.github.com>
2025-05-09 13:33:38 +0100

    test

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ const difficile = [
 
 
 let musicGame = new Audio("Media/gameMusic.mp3");
-musicGame.setAttribute("preload", "none");
+musicGame.preload = none;
 musicGame.loop = true;
 musicGame.volume = 0.04;
 musicGame.play();
